fc683faeafe9edc52c1cce393c34159bf94b6b54
MathieuAA <mathieu.aa@free.fr>
2015-05-19 17:01:23 +0200

    JHipster-UML now generates entities, before it was cool

--- entitiescreator.js
+++ entitiescreator.js
@@ -4,7 +4,7 @@ var xml2js = require('xml2js'), // for reading and parsing the XMI
     chalk = require('chalk'),
     fs = require('fs'),
     _s = require('underscore.string'),
-	jf = require('jsonfile'),
+	  jf = require('jsonfile'),
     types = require('./types');
 	
 	// constants used throughout the script
@@ -160,52 +160,50 @@ EntitiesCreator.prototype.setValidationsOfField = function(field, fieldId, class
  *    on the other side of the association
  */
 EntitiesCreator.prototype.setRelationshipOfEntity = function(classId) {
- for(var k in this.classes[classId].injectedFields){
+ for(var k in this.classes[classId].injectedFields) {
       var injectId= this.classes[classId].injectedFields[k];
-  
+
       /* fill the ownerSide relationship */
       var relationshipOwnerSide = {
-        "relationshipId"    : this.entities[classId].relationships.length + 1,
-        "relationshipName"  : upperCase(this.injectedFields[injectId].name),
-        "relationshipNameCapitalized" : upperCase(this.injectedFields[injectId].name),
-        "relationshipFieldName" : this.injectedFields[injectId].name,
+        "relationshipId"    : this.entities[classId].relationships.length +1,
+        "relationshipName"  : upperCase(this.classes[this.injectedFields[injectId].type].name),
+        "relationshipNameCapitalized" : upperCase(this.classes[this.injectedFields[injectId].type].name),
+        "relationshipFieldName" : lowerCase(this.classes[this.injectedFields[injectId].type].name), // this.injectedFields[injectId].name,
         "otherEntityName"   : lowerCase(this.classes[this.injectedFields[injectId].type].name),
         "relationshipType"  : this.injectedFields[injectId].cardinality,
         "otherEntityNameCapitalized" : upperCase(this.classes[this.injectedFields[injectId].type].name)
-      }
+      };
   
       /*fill the relationship on the other side of the association acording to the relationType*/
-      //TODO GERE LA REFLEXIVIT2
+      //TODO make the reflexive associations work
       var relationshipOtherSide = {
-          "relationshipId"    : this.entities[this.injectedFields[injectId].type].relationships.length +1,
-          "relationshipName"  : this.getAssociationFieldName(this.injectedFields[injectId].association, upperCase),
-          "relationshipNameCapitalized" : this.getAssociationFieldName(this.injectedFields[injectId].association, upperCase) ,
-          "relationshipFieldName" : this.getAssociationFieldName(this.injectedFields[injectId].association, lowerCase), /*TODO*/  //Le nom du champs est par defaut le nom de l'autre classe en minuscule
+          "relationshipId"    : this.entities[this.injectedFields[injectId].type].relationships.length + 1,
+          "relationshipName" : upperCase(this.classes[this.associations[this.injectedFields[injectId].association].type].name),
+          "relationshipNameCapitalized" : upperCase(this.classes[this.associations[this.injectedFields[injectId].association].type].name),
+          "relationshipFieldName" : lowerCase(this.classes[this.injectedFields[injectId].class].name), //this.getAssociationFieldName(this.injectedFields[injectId].association, lowerCase),
           "otherEntityName"   : lowerCase(this.classes[this.associations[this.injectedFields[injectId].association].type].name),
           "otherEntityNameCapitalized" : upperCase(this.classes[this.associations[this.injectedFields[injectId].association].type].name)
-      }
-      if(relationshipOwnerSide.relationshipType == ONE_TO_ONE){
-        relationshipOwnerSide.ownerSide = true;
-    
+      };
 
-        relationshipOtherSide ["relationshipType"]=ONE_TO_ONE;
-        relationshipOtherSide ["ownerSide"]=false;
-
-      }else if(relationshipOwnerSide.relationshipType == ONE_TO_MANY){
-        this.entities[classId].fieldsContainOneToMany=true;
-
-        relationshipOtherSide["relationshipType"]= MANY_TO_ONE;
-        relationshipOtherSide["otherEntityField"]='id'; //By default set at 'id'
-  
-      }else if(relationshipOwnerSide.relationshipType == MANY_TO_MANY){
-        relationshipOwnerSide.ownerSide = true;
-
-        this.entities[classId].fieldsContainOwnerManyToMany=true;
-
-        relationshipOtherSide ["relationshipType"]=MANY_TO_MANY;
-        relationshipOtherSide ["ownerSide"]=false;
-
-        this.entities[classId]["fieldsContainOwnerManyToMany"]=true;
+      switch(relationshipOwnerSide.relationshipType) {
+        case ONE_TO_ONE:
+          relationshipOwnerSide.ownerSide = true;
+          relationshipOtherSide["relationshipType"] = ONE_TO_ONE;
+          relationshipOtherSide["ownerSide"] = false;
+          break;
+        case ONE_TO_MANY:
+          this.entities[classId].fieldsContainOneToMany = true;
+          relationshipOtherSide["relationshipType"] = MANY_TO_ONE;
+          relationshipOtherSide["otherEntityField"] = 'id'; //By default set at 'id'
+          break;
+        case MANY_TO_MANY:
+          relationshipOwnerSide.ownerSide = true;
+          relationshipOwnerSide['otherEntityField'] = 'id';
+          this.entities[classId].fieldsContainOwnerManyToMany = true;
+          relationshipOtherSide["relationshipType"] = MANY_TO_MANY;
+          relationshipOtherSide["ownerSide"] = false;
+          this.entities[classId]["fieldsContainOwnerManyToMany"] = true;
+          break;
       }
   
       this.entities[classId].relationships.push(relationshipOwnerSide);
@@ -223,10 +221,10 @@ EntitiesCreator.prototype.setRelationshipOfEntity = function(classId) {
  */
 EntitiesCreator.prototype.writeJSON = function() {
   if(!fs.existsSync('.jhipster')){
-        fs.mkdirSync('.jhipster');
-   }
+    fs.mkdirSync('.jhipster');
+  }
 
-  for(var k in this.entities){
+  for(var k in this.entities) {
     var file = '.jhipster/'+this.classes[k].name+'.json';
     jf.writeFileSync(file, this.entities[k]);    
   }
