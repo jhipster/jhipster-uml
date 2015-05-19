22b5b389ceeda264185268b82ec0184e7adbf410
CarlKlagba <klagba@gmail.com>
2015-05-19 10:13:23 +0200

    execute command for each JSON Entity

--- jhipster-uml.js
+++ jhipster-uml.js
@@ -9,6 +9,7 @@ if (process.argv.length < 4) {
 
 var fs = require('fs'),
 	chalk = require('chalk'),
+  child_process = require('child_process'),
 	XMIParser = require('./xmiparser'),
 	EntitiesCreator = require('./entitiescreator'),
   ClassScheduler = require('./scheduler');
@@ -31,6 +32,44 @@ var scheduler = new ClassScheduler(
 scheduler.schedule();
 
 var scheduledClasses = scheduler.getOrderedPool();
+
+createEntities(scheduledClasses, parser.getClasses());
+/**
+ * Execute the command yo jhipster:entity for all the classes in the right order
+ */
+function createEntities(scheduledClasses, classes){
+
+for (var i =0; i< scheduledClasses.length; i++) {
+  console.log(chalk.red(classes[scheduledClasses[i]].name));
+};
+
+ var i= 0;
+ executeEntity(scheduledClasses, classes, i);
+
+}
+function executeEntity(scheduledClasses, classes, index){
+   var child;
+  console.log(chalk.red("================= "+classes[scheduledClasses[index]].name+ " ================="));
+  child =child_process.exec("yo jhipster:entity "+classes[scheduledClasses[index]].name, function (error, stdout, stderr) {
+
+    console.log(chalk.green(stdout));
+    console.log();
+     if (error !== null) {
+      console.log(chalk.red(error));
+    }
+
+    if(stderr == '' || stderr != null){
+      console.log( stderr);
+    }
+
+    //the end condition
+    if(index+1 >= scheduledClasses.length){
+      return;
+    }
+    executeEntity(scheduledClasses, classes, index+1);
+  });
+}
+
 
 function NoArgumentSuppliedException(message) {
   this.name = 'NoArgumentSuppliedException';
