d26cd9943673eea851947e49f5d4ddad764af3af
CarlKlagba <klagba@gmail.com>
2015-05-22 15:39:43 +0200

    Bug fix: crashes when a class has no attributs

--- xmiparser.js
+++ xmiparser.js
@@ -184,7 +184,11 @@ XMIParser.prototype.fillAssociations = function() {
 XMIParser.prototype.fillClassesAndFields = function() {
   for (var i = 0; i < this.rawClassesIndexes.length; i++) {
     var element = this.root.packagedElement[this.rawClassesIndexes[i]];
-    this.addClass(element);   
+    this.addClass(element);
+
+    if (element.ownedAttribute == undefined) {
+      continue;
+    }
 
     for (var j = 0; j < element.ownedAttribute.length; j++) {
       if (!this.isAnId(
