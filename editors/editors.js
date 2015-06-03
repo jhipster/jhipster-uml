5ad1c1f85eb8eac6534c5560a39298af4af86621
MathieuAA <mathieu.aa@free.fr>
2015-06-03 17:40:23 +0200

    Bug fix The GENMYMODEL constant has been added

--- editors/editors.js
+++ editors/editors.js
@@ -1,14 +1,15 @@
 'use strict';
 
 var modelio_parser = require('./modelio_parser'),
-	genmymodel_parser = require('./genmymodel_parser'),
+	  genmymodel_parser = require('./genmymodel_parser'),
     umldesigner_parser = require('./umldesigner_parser');
 
 exports.MODELIO = 'modelio';
 exports.UMLDESIGNER = 'umldesigner';
+exports.GENMYMODEL = 'genmymodel';
 
 exports.Parsers = {
   modelio: modelio_parser.ModelioParser,
   umldesigner: umldesigner_parser.UMLDesigner,
-  genmymodel:genmymodel_parser.GenMyModelParser
+  genmymodel: genmymodel_parser.GenMyModelParser
 };
