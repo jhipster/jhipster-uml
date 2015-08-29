5d365e5f26908fd7eeb13821cac16f4e42a3111d
MathieuAA <mathieu.aa@free.fr>
2015-08-29 14:10:39 +0200

    The VP file parser's migration is done

--- test/visualparadigm_parser_test.js
+++ test/visualparadigm_parser_test.js
@@ -2,7 +2,6 @@
 
 var chai = require('chai'),
     expect = chai.expect,
-    mp = require('../lib/editors/visualparadigm_parser'),
     ParserFactory = require('../lib/editors/parser_factory');
 
 var parser = ParserFactory.createParser('./test/xmi/visualparadigm.uml', 'sql');
@@ -91,15 +90,15 @@ describe('VisualParadigmParser', function() {
 
       it('assigns their id with their capitalized name', function() {
         var expectedTypes = [ 'BigDecimal', 'DateTime', 'Long' ];
-        for(var element in parser.getTypes()) {
-          if(parser.getTypes().hasOwnProperty(element)) {
+        Object.keys(parser.parsedData.types).forEach(function(type) {
+          if(parser.parsedData.types.hasOwnProperty(type)) {
             expect(
               expectedTypes
-            ).to.include(parser.getTypes()[element]);
+            ).to.include(parser.parsedData.getType(type).name);
             expectedTypes.splice(
-              expectedTypes.indexOf(parser.getTypes()[element]), 1);
+              expectedTypes.indexOf(parser.parsedData.getType(type).name), 1);
           }
-        }
+        });
         expect(expectedTypes.length).to.equal(0);
       });
     });
@@ -109,10 +108,10 @@ describe('VisualParadigmParser', function() {
         var otherParser =
           ParserFactory.createParser('./test/xmi/visualparadigm.uml', 'sql');
         otherParser.fillTypes();
-        Object.keys(otherParser.getTypes()).forEach(function(type) {
+        Object.keys(otherParser.parsedData.types).forEach(function(type) {
           expect(
-            otherParser.getTypes()[type].name
-          ).to.equal(_.capitalize(otherParser.getTypes()[type].name));
+            parser.parsedData.getType(type).name
+          ).to.equal(_.capitalize(parser.parsedData.getType(type).name));
         });
       });
     });
@@ -157,9 +156,9 @@ describe('VisualParadigmParser', function() {
         var expectedNValues = ['VALUE_A', 'VALUE_A', 'VALUE_B'];
         var names = [];
         var values = [];
-        Object.keys(otherParser.getEnums()).forEach(function(element) {
-          names.push(otherParser.getEnums()[element].name);
-          otherParser.getEnums()[element].values.forEach(function(value) {
+        Object.keys(otherParser.parsedData.enums).forEach(function(enumId) {
+          names.push(otherParser.parsedData.getEnum(enumId).name);
+          otherParser.parsedData.getEnum(enumId).values.forEach(function(value) {
             values.push(value);
           });
         });
@@ -205,78 +204,24 @@ describe('VisualParadigmParser', function() {
 
     describe('#addClass', function() {
       it('adds the found classes', function() {
-        expect(Object.keys(parser.getClasses()).length).to.equal(9);
-      });
-
-      describe("when trying to access an element's attributes", function() {
-        var firstElementKeys;
-
-        before(function() {
-          firstElementKeys = Object.keys(
-            parser.getClasses()[Object.keys(parser.getClasses())[0]]);
-        });
-
-        it('has a name', function() {
-          expect(firstElementKeys).to.include('name');
-        });
-
-        it('has fields', function() {
-          expect(firstElementKeys).to.include('fields');
-        });
-
-        it('has injected fields', function() {
-          expect(firstElementKeys).to.include('injectedFields');
-        });
-
-        it('should not throw any error if there is no attribute', function() {
-          var anotherParser = ParserFactory.createParser(
-            './test/xmi/visualparadigm_no_attribute_test.uml',
-            'sql');
-          try {
-            anotherParser.parse();
-          } catch (error) {
-            throw new ExpectationError();
-          }
-        });
+        expect(Object.keys(parser.parsedData.classes).length).to.equal(9);
       });
     });
 
     describe('#addField', function() {
       describe('#addRegularField', function() {
         it('adds the fields', function() {
-          expect(Object.keys(parser.getFields()).length).to.equal(21);
+          expect(Object.keys(parser.parsedData.fields).length).to.equal(21);
         });
 
         it('adds the fields to the classes', function() {
           var count = 0;
-          for(var element in parser.getClasses()) {
-            if (parser.getClasses().hasOwnProperty(element)) {
-              count += parser.getClasses()[element]['fields'].length;
+          Object.keys(parser.parsedData.classes).forEach(function(element) {
+            if (parser.parsedData.classes.hasOwnProperty(element)) {
+              count += parser.parsedData.getClass(element).fields.length;
             }
-          }
-          expect(count).to.equal(Object.keys(parser.getFields()).length);
-        });
-
-        describe("when trying to access an element's attributes", function() {
-          var firstElementKeys;
-
-          before(function() {
-            firstElementKeys = Object.keys(
-              parser.getFields()[
-                Object.keys(parser.getFields())[0]]);
           });
-
-          it('has a name', function() {
-            expect(firstElementKeys).to.include('name');
-          });
-
-          it('has validations', function() {
-            expect(firstElementKeys).to.include('validations');
-          });
-
-          it('has a type', function() {
-            expect(firstElementKeys).to.include('type');
-          });
+          expect(count).to.equal(Object.keys(parser.parsedData.fields).length);
         });
 
         describe('when having an invalid type in the XMI', function() {
@@ -297,7 +242,7 @@ describe('VisualParadigmParser', function() {
             'when a type was not defined in a primitiveType tag',
             function() {
           it('is deduced from the field element, and added', function() {
-            expect(parser.getTypes()['String']).to.equal('String');
+            expect(parser.parsedData.getType('String').name).to.equal('String');
           });
         });
       });
@@ -306,67 +251,12 @@ describe('VisualParadigmParser', function() {
 
   describe('#fillAssociations', function() {
     it('inserts the found associations', function() {
-      expect(Object.keys(parser.getAssociations()).length).to.equal(10);
-    });
-
-    describe("when trying to access an element's attributes", function() {
-      var firstElementKeys;
-
-      before(function() {
-        firstElementKeys = Object.keys(
-          parser.getAssociations()[Object.keys(parser.getAssociations())[0]]);
-      })
-
-      it('has a name', function() {
-        expect(firstElementKeys).to.include('name');
-      });
-
-      it('has a type', function() {
-        expect(firstElementKeys).to.include('type');
-      });
-
-      it('has a flag telling if it has an upper value', function() {
-        expect(firstElementKeys).to.include('isUpperValuePresent');
-      });
+      expect(Object.keys(parser.parsedData.associations).length).to.equal(10);
     });
 
     describe('#addInjectedField', function() {
       it('adds the injected fields', function() {
-        expect(Object.keys(parser.getInjectedFields()).length).to.equal(10);
-      });
-
-      describe("when trying to access an element's attributes", function() {
-        var firstElementKeys;
-
-        before(function() {
-          firstElementKeys = Object.keys(
-            parser.getInjectedFields()[
-              Object.keys(parser.getInjectedFields())[0]]);
-        });
-
-        it('has a name', function() {
-          expect(firstElementKeys).to.include('name');
-        });
-
-        it('has a type', function() {
-          expect(firstElementKeys).to.include('type');
-        });
-
-        it('has a association', function() {
-          expect(firstElementKeys).to.include('association');
-        });
-
-        it('has a class', function() {
-          expect(firstElementKeys).to.include('class');
-        });
-
-        it('has a flag if the upper value is present', function() {
-          expect(firstElementKeys).to.include('isUpperValuePresent');
-        });
-
-        it('has a cardinality', function() {
-          expect(firstElementKeys).to.include('cardinality');
-        });
+        expect(Object.keys(parser.parsedData.injectedFields).length).to.equal(10);
       });
     });
   });
