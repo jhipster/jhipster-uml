799e27cc3767c0bcd09cab12e1b7e37735ab3cf8
MathieuAA <mathieu.aa@free.fr>
2015-09-23 20:20:53 +0200

    The tests are fixed

--- test/entity_generator_test.js
+++ test/entity_generator_test.js
@@ -6,25 +6,6 @@ var chai = require('chai'),
 
 describe('#generateEntities', function() {
   describe('when generating the entities', function() {
-    describe('with invalid parameters', function() {
-      describe('such as invalid classes', function() {
-        it("throws an exception because they don't have the same content", function() {
-          try {
-            generateEntities([], { key: 'value' });
-            throw new ExpectationError();
-          } catch (error) {
-            expect(error.name).to.equal('IllegalArgumentException');
-          }
-          try {
-            generateEntities([ 'element' ], {});
-            throw new ExpectationError();
-          } catch (error) {
-            expect(error.name).to.equal('IllegalArgumentException');
-          }
-        });
-      });
-    });
-
     describe('with valid parameters', function() {
       describe('such as empty classes', function() {
         it("doesn't throw any error", function() {
