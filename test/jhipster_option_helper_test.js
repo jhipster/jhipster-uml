4f4d9450ed465abecd84a80a4df8b98673d2ab30
MathieuAA <mathieu.aa@free.fr>
2015-12-07 22:05:09 +0100

    Now possible to specify a service in the JDL

--- test/jhipster_option_helper_test.js
+++ test/jhipster_option_helper_test.js
@@ -3,7 +3,8 @@
 var expect = require('chai').expect,
     fail = expect.fail,
     isAValidDTO = require('../lib/helper/jhipster_option_helper').isAValidDTO,
-    isAValidPagination = require('../lib/helper/jhipster_option_helper').isAValidPagination;
+    isAValidPagination = require('../lib/helper/jhipster_option_helper').isAValidPagination,
+    isAValidService = require('../lib/helper/jhipster_option_helper').isAValidService;
 
 describe('#isAValidDTO', function() {
   describe('when passing a valid DTO option', function() {
@@ -29,6 +30,20 @@ describe('#isAValidPagination', function() {
   describe('when passing an invalid pagination option', function() {
     it('returns false', function() {
       expect(isAValidPagination('not a valid pagination option!!')).to.be.false;
+    });
+  });
+});
+
+describe('#isAValidService', function() {
+  describe('when passing a valid service option', function() {
+    it('returns true', function() {
+      expect(isAValidService('serviceClass')).to.be.true;
+    });
+  });
+
+  describe('when passing an invalid service option', function() {
+    it('returns false', function() {
+      expect(isAValidService('not a valid service option!!')).to.be.false;
     });
   });
 });
