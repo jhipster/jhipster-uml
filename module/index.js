7e3dea2e8119614b3970a8759bb7cd166c0e0bec
CarlKLagba <klagba@gmail.fr>
2015-12-26 17:56:00 +0100

    Export enough for the UML edit

---
+++ module/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var editors = require('../lib/editors/editors'),
+	EntitiesCreator = require('../lib/entitiescreator'),
+	ClassScheduler = require('./lib/scheduler'),
+	SQLTypes = require('../lib/types/sql_types'),
+    MongoDBTypes = require('../lib/types/mongodb_types'),
+    CassandraTypes = require('../lib/types/cassandra_types');
+
+module.exports = (function(){
+    var self = {};
+   	self.editors = editors;
+   	self.EntitiesCreator = EntitiesCreator;
+   	self.ClassScheduler = ClassScheduler;
+
+   	self.SQLTypes = new SQLTypes();
+   	self.MongoDBTypes = new MongoDBTypes();
+   	self.CassandraTypes = new CassandraTypes();
+
+    return self;
+})();
