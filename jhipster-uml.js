bfcc916c5ac2072cd51c3bf337f54d5d89e00f9d
MathieuAA <mathieu.aa@free.fr>
2016-02-06 23:26:43 +0100

    Work done for #135

--- jhipster-uml.js
+++ jhipster-uml.js
@@ -7,6 +7,9 @@ try {
   require('./lib/jhipsteruml');
 } catch (error) {
   console.error(chalk.red('An error has occurred:\n\t') + error.name);
+  if (error.name === 'SyntaxError') {
+    console.error(chalk.red('At line ' + error.location.start.line + ', column ' + error.location.start.column + '.'));
+  }
   console.error(chalk.red('Error message:\n\t') + error.message);
   console.error(chalk.red('Stack trace:\n') + error.stack);
 }
