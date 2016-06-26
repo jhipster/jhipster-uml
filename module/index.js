11e4c5cb26c2ef57768351ed988ecb912f2d2843
Mathieu Abou-Aichi <mathieu.aa@free.fr>
2016-06-26 19:50:40 +0200

    Updated the API (#189)

--- module/index.js
+++ module/index.js
@@ -1,28 +1,17 @@
 'use strict';
 
-const editors = require('../lib/editors/editors'),
-  entityGenerator = require('../lib/entity_generator'),
-  EntitiesCreator = require('../lib/entitiescreator'),
-  ClassScheduler = require('../lib/scheduler'),
+const Editors = require('../lib/editors/editors'),
+    createParser = require('../lib/editors/parser_factory').createParser,
+    Parser = require('../lib/editors/parser'),
+    AbstractParser = require('../lib/editors/abstract_parser'),
+    entityGenerator = require('../lib/entity_generator').generateEntities,
+    EntitiesCreator = require('../lib/entitiescreator').createEntities;
 
-  SQLTypes = require('../lib/types/sql_types'),
-  MongoDBTypes = require('../lib/types/mongodb_types'),
-  CassandraTypes = require('../lib/types/cassandra_types'),
-  cardinalities = require('../lib/cardinalities');
-
-
-module.exports = (function(){
-  var self = {};
-  self.editors = editors;
-  self.entityGenerator = entityGenerator;
-  self.EntitiesCreator = EntitiesCreator;
-  self.ClassScheduler = ClassScheduler;
-
-
-  self.SQLTypes = new SQLTypes();
-  self.MongoDBTypes = new MongoDBTypes();
-  self.CassandraTypes = new CassandraTypes();
-  self.cardinalities = cardinalities;
-
-  return self;
-})();
+module.exports = {
+  Editors: Editors,
+  createParser: createParser,
+  Parser: Parser,
+  AbstractParser: AbstractParser,
+  createEntities: createEntities,
+  generateEntities: generateEntities
+};
