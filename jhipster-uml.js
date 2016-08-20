650540bc6d4cb983117f38a8c03bcd0bd6a06e53
Mathieu Abou-Aichi <mathieu.aa@free.fr>
2016-08-20 22:47:32 +0200

    Used string interpolation

--- jhipster-uml.js
+++ jhipster-uml.js
@@ -6,14 +6,14 @@ const chalk = require('chalk');
 try {
   require('./lib/jhipsteruml');
 } catch (error) {
-  console.error(chalk.red('An error has occurred:\n\t') + error.name);
+  console.error(`${chalk.red('An error has occurred:\n\t')}${error.name}`);
   if (error.name === 'SyntaxError') {
     console.error(
         chalk.red(
             `At line ${error.location.start.line }, column ${error.location.start.column}.`));
   }
-  console.error(chalk.red('Error message:\n\t') + error.message);
+  console.error(`${chalk.red('Error message:\n\t')}${error.message}`);
   if (error.stack) {
-    console.error(chalk.red('Stack trace:\n') + error.stack);
+    console.error(`${chalk.red('Stack trace:\n')}${error.stack}`);
   }
 }
