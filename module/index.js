220d6884a6f5974b5be1e9b0671239cfcf94b71e
Mathieu Abou-Aichi <mathieu.aa@free.fr>
2016-10-09 18:22:23 +0200

    Removed useless files

--- module/index.js
+++ module/index.js
@@ -2,8 +2,6 @@
 
 const Editors = require('../lib/editors/editors'),
     createParser = require('../lib/editors/parser_factory').createParser,
-    Parser = require('../lib/editors/parser'),
-    AbstractParser = require('../lib/editors/abstract_parser'),
     generateEntities = require('../lib/entity_generator').generateEntities,
     createEntities = require('../lib/entitiescreator').createEntities,
     JDLExporter = require('../lib/export/jdl_exporter');
@@ -11,8 +9,6 @@ const Editors = require('../lib/editors/editors'),
 module.exports = {
   Editors: Editors,
   createParser: createParser,
-  Parser: Parser,
-  AbstractParser: AbstractParser,
   createEntities: createEntities,
   generateEntities: generateEntities,
   toJDL: JDLExporter.toJDL,
