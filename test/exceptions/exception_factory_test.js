3f7bcbe80092a8c4ad000f650ba2a455e8b7d90d
Mathieu Abou-Aichi <mathieu.aa@free.fr>
2016-10-09 18:13:25 +0200

    Cleaned up the exception test

--- test/exceptions/exception_factory_test.js
+++ test/exceptions/exception_factory_test.js
@@ -1,51 +1,56 @@
 'use strict';
 
 const expect = require('chai').expect,
-    buildException = require('../../lib/exceptions/exception_factory').buildException,
-    exceptions = require('../../lib/exceptions/exception_factory').exceptions;
+    buildException = require('../../lib/exceptions/exception_factory').buildException;
 
-describe('ExceptionFactory', function() {
-  describe('#buildException', function() {
-    it("adds the 'Exception' suffix to the names", function() {
-      let exception = buildException('Working', null);
+describe('ExceptionFactory', () => {
+  describe('#buildException', () => {
+    let exception = buildException('Working', null);
+
+    it("adds the 'Exception' suffix to the names", () => {
       expect(exception.name).to.eq('WorkingException');
     });
-    it('builds throwable objects', function() {
+    it('builds throwable objects', () => {
       try {
         throw new buildException('Working', null);
       } catch (error) {
+        expect(error.name).to.eq('WorkingException');
       }
     });
-    describe('when only passing a name', function() {
-      it('takes the name and adds no message', function() {
-        let exception1 = buildException('Working', null);
-        let exception2 = buildException('Working', '');
+    describe('when only passing a name', () => {
+      let exception1 = buildException('Working', null);
+      let exception2 = buildException('Working', '');
+
+      it('takes the name and adds no message', () => {
         expect(exception1.name).to.eq('WorkingException');
         expect(exception1.message).to.be.empty;
         expect(exception2.name).to.eq('WorkingException');
         expect(exception2.message).to.be.empty;
-      })
+      });
     });
-    describe('when only passing a message', function() {
-      it("just adds the suffix and keeps the message", function() {
-        let exception1 = buildException(null, 'The message');
-        let exception2 = buildException('', 'The message');
+    describe('when only passing a message', () => {
+      let exception1 = buildException(null, 'The message');
+      let exception2 = buildException('', 'The message');
+
+      it('just adds the suffix and keeps the message', () => {
         expect(exception1.name).to.eq('Exception');
         expect(exception1.message).to.eq('The message');
         expect(exception2.name).to.eq('Exception');
         expect(exception2.message).to.eq('The message');
-      })
+      });
     });
-    describe('when passing in a name and a message', function() {
-      it('keeps both', function() {
-        let exception = buildException('Good', 'The message');
+    describe('when passing in a name and a message', () => {
+      let exception = buildException('Good', 'The message');
+
+      it('keeps both', () => {
         expect(exception.name).to.eq('GoodException');
         expect(exception.message).to.eq('The message');
       });
     });
-    describe('when not passing anything', function() {
-      it("names the exception 'Exception' and puts no message", function() {
-        let exception = buildException(null, null);
+    describe('when not passing anything', () => {
+      let exception = buildException(null, null);
+
+      it("names the exception 'Exception' and puts no message", () => {
         expect(exception.name).to.eq('Exception');
         expect(exception.message).to.be.empty;
       });
