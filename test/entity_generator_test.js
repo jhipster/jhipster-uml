2aee1baf74da82ba778afc8eddf415a2ab83ae4e
MathieuAA <mathieu.aa@free.fr>
2017-11-27 20:58:14 +0100

    Refactored the entity generator and added tests

--- test/entity_generator_test.js
+++ test/entity_generator_test.js
@@ -1,20 +1,65 @@
-const expect = require('chai').expect;
+/* eslint-disable no-unused-expressions */
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const childProcess = require('child_process');
 
 const generateEntities = require('../lib/entity_generator').generateEntities;
 
-describe('#generateEntities', () => {
-  describe('when generating the entities', () => {
-    describe('with valid parameters', () => {
-      describe('such as empty classes', () => {
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('EntityGenerator', () => {
+  describe('::generateEntities', () => {
+    context('when trying to generate with empty parameters', () => {
+      context('such as empty entities to generate', () => {
         it('doesn\'t throw any error', () => {
-          expect(generateEntities([], {}));
+          expect(generateEntities([], { toto: { name: 'toto' } }, ['toto']));
         });
       });
-
-      describe('with non-empty classes', () => {
+      context('such as empty classes', () => {
         it('doesn\'t throw any error', () => {
-          // can't test it because it is dependant on JHipster
+          expect(generateEntities(['toto'], {}, ['toto']));
         });
+      });
+      context('such as empty entity names to generate', () => {
+        it('doesn\'t throw any error', () => {
+          expect(generateEntities(['toto'], { toto: { name: 'toto' } }, []));
+        });
+      });
+    });
+    context('when generating with regular parameters', () => {
+      let spawnSyncStub = null;
+
+      before(() => {
+        spawnSyncStub = sinon.stub(childProcess, 'spawnSync').returns(null);
+        generateEntities(
+          ['toto', 'titi'],
+          { toto: { name: 'toto' }, titi: { name: 'titi' } },
+          ['toto', 'titi'],
+          {
+            force: true,
+            listOfNoClient: ['titi'],
+            listOfNoServer: ['toto'],
+            fluentMethods: ['titi', 'toto'],
+            angularSuffixes: { titi: 'titiSuffix' },
+            noUserManagement: true
+          });
+      });
+      after(() => {
+        spawnSyncStub.restore();
+      });
+
+      it('uses twice the subgen', () => {
+        expect(spawnSyncStub).to.have.been.calledTwice;
+      });
+      it('calls the subgen with the specific options', () => {
+        expect(
+          `${spawnSyncStub.getCall(0).args[0]} ${spawnSyncStub.getCall(0).args[1].join(' ')}`
+        ).to.equal('yo jhipster:entity toto --force --skip-server --no-fluent-methods --skip-user-management --skip-install --regenerate');
+        expect(
+          `${spawnSyncStub.getCall(1).args[0]} ${spawnSyncStub.getCall(1).args[1].join(' ')}`
+        ).to.equal('yo jhipster:entity titi --force --skip-client --no-fluent-methods --angular-suffix titiSuffix --skip-user-management --regenerate');
       });
     });
   });
